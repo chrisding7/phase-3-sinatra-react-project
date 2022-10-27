4ce05e2ac363d1047ab28629b5a8e220300bf2e4
Chris Ding <chrisding7@berkeley.edu>
2022-10-27 14:59:31 -0700

    switched db from sqlite3 to posgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_21_183352) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_21_183352) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "entries", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
     t.string "location"
     t.string "description"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "entry_id"
+    t.bigint "user_id"
+    t.bigint "entry_id"
     t.index ["entry_id"], name: "index_favorites_on_entry_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
